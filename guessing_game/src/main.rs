8595e860b5aa0fa6fe7997c7f1d32910948dfbf8
Chris <chris.john.madden@gmail.com>
2021-03-11 14:17:12 +0000

    UPDATE read value in from command line

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+// Bring io library into scope
+// io library is comes from standard library
+use std::io;
+
+// Entry point into the program
+// fn declares new function
 fn main() {
-    println!("Hello, world!");
+
+    // this is a macro
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // 'let' is used to create a variable
+    // Variables are immutable by default
+    // 'mut' allows the variable to be mutable
+    // 'String' is a variable type
+    // '::' indicated new is an associated function of the String type
+    // Some languages call this a static method
+    let mut guess = String::new();
+
+    // Call the stdin() function from the io library
+    io::stdin()
+        .read_line(&mut guess) // &mut indicates the argument is a reference 
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\chris_ek76h2p\Projects\Rust\RUST-Guessing-Game\guessing_game\target\debug\guessing_game.exe: C:\Users\chris_ek76h2p\Projects\Rust\RUST-Guessing-Game\guessing_game\src\main.rs
