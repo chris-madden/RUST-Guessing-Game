b31679b07cd4a92b4b73e7d94c3ff3a27fca3907
Chris <chris.john.madden@gmail.com>
2021-03-11 15:04:35 +0000

    UPDATE error handling added, game complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,8 +13,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("Secret number generated {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
@@ -31,8 +29,11 @@ fn main() {
             .read_line(&mut guess) // &mut indicates the argument is a reference
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
